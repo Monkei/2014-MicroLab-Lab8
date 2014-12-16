0586b46d7d356b7ec9f1dcacc6b4873cd1ed3380
HWHung <howard830111@yahoo.com.tw>
2014-12-16 15:32:01 +0800

    Create Lab8-2(9600).c

---
+++ Lab8-2(9600).c
@@ -0,0 +1,97 @@
+#include<REG_MPC82G516.H>
+
+void delay_us(int us);
+void LOOP(); 
+
+unsigned char input, output;
+
+void main()
+{
+	T2CON = 0x34;
+	//TMOD = 0x20;		//MOV TMOD, #00100000B
+	PCON &= 0xFF;	//ANL PCON, #11111111B
+	TH2 = 0xFF;			//MOV TH1, #F3H
+	TL2 = 0xD9;			//MOV TL1, #F3H
+	RCAP2H = 0xFF;
+	RCAP2L = 0xD9;
+	TR2 = 1;			//SETB TR1
+	SCON = 0xD0;
+
+	
+	while(1)
+	{
+		input = '\0';
+		while(input == '\0'){
+			if(RI == 1)
+			{
+				RI = 0;
+				input = SBUF;
+			}
+		}
+		
+		switch (input)
+		{
+			case 'q':case 'Q':
+				output = 'Q';
+				LOOP();
+				output = 'A';
+				LOOP();
+				output = 'Q';
+				LOOP();
+				break;
+			case 'w':case 'W':
+				output = 'w';
+				LOOP();
+				output = 'w';
+				LOOP();
+				output = 'w';
+				LOOP();
+				output = 'w';
+				LOOP();
+				output = 'w';
+				LOOP();
+				break;
+			case 'e':case 'E':
+				output = 'N';
+				LOOP();
+				output = 'C';
+				LOOP();
+				output = 'T';
+				LOOP();
+				output = 'U';
+				LOOP();
+				break;
+			case 'r':case 'R':
+				output = 'C';
+				LOOP();
+				output = 'S';
+				LOOP();
+				output = 'I';
+				LOOP();
+				output = 'E';
+				LOOP();
+				break;
+		}
+	}
+	
+}
+
+void LOOP()
+{
+	SBUF = output;
+	delay_us(50);
+	while(TI == 0);
+	TI = 0;
+}
+
+
+
+void delay_us(int us)
+{                  
+		unsigned int i;
+   while(us--)
+	 {
+			i = 1000;
+			while(i--);
+	 }
+}
